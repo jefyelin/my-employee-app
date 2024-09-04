37873d507dfa0fe3cd0586056ee4a9a061d4bd91
jefyelin <39713463+jefyelin@users.noreply.github.com>
2024-09-04 14:02:09 -0300

    enable MSW in production build

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import "@/styles/globals.css";
 import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
-import "@/styles/globals.css";
 
 async function enableMocking() {
-  if (process.env.NODE_ENV !== "development") {
-    return;
-  }
-
   const { worker } = await import("./mocks/browser");
 
   return worker.start();
